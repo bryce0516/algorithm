3b0cea7dad0da4c985085bdbb541a52ad87b159c
hyesungpark <jayhspark@gmail.com>
2021-08-20 14:01:36 +0900

    update

--- order.js
+++ order.js
@@ -1,13 +1,37 @@
-function solution(n) {
-  const newArr = n.toString().split("").sort(compare);
-  function compare(a, b) {
-    return b - a;
-  }
-  // const redu = newArr.reduce(reducer);
-  const re = newArr.join("");
+// function solution(n) {
+//   const newArr = n.toString().split("").sort(compare);
+//   function compare(a, b) {
+//     return b - a;
+//   }
+//   // const redu = newArr.reduce(reducer);
+//   const re = newArr.join("");
 
-  var answer = Number(re);
+//   var answer = Number(re);
+//   return answer;
+// }
+
+function solution(n) {
+  let isPositive = true;
+  if (n < 0) {
+    n = Math.abs(n);
+    isPositive = false;
+  }
+  const newArr = isPositive
+    ? n.toString().split("").reverse()
+    : n
+        .toString()
+        .split("")
+        .map((e) => {
+          return `-${e}`;
+        })
+        .reverse();
+
+  // const redu = newArr.reduce(reducer);
+  const map1 = newArr.map((element) => Number(element));
+
+  var answer = map1;
   return answer;
 }
+const d = solution(-123123);
 
-solution(118372);
+// console.log(d);
