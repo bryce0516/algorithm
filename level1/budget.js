6279a56738a6ca3c426a55fff156f844e47ac6d8
Hyesung Park <jayhspark@gmail.com>
2021-09-09 15:15:34 +0900

    update

--- level1/budget.js
+++ level1/budget.js
@@ -1,7 +1,33 @@
-console.log("works");
 function solution(d, budget) {
-  var answer = 0;
+  function compare(a, b) {
+    return a - b;
+  }
+  const sortedArr = d.sort(compare);
+
+  const recursive = (arr, budget) => {
+    console.log("recursive arr", arr);
+    const plus = arr.reduce((acc, cur) => {
+      acc += cur;
+      return acc;
+    }, 0);
+    if (plus > budget) {
+      arr.pop();
+      return recursive(arr, budget);
+    } else {
+      return arr.length;
+    }
+  };
+  const answer = recursive(sortedArr, budget);
+  console.log(answer);
   return answer;
 }
 
 solution([1, 3, 2, 5, 4], 9);
+
+function solution(d, budget) {
+  d.sort((a, b) => a - b);
+
+  while (d.reduce((a, b) => a + b, 0) > budget) d.pop();
+
+  return d.length;
+}
