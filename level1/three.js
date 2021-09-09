13a8948c0abe2cd822212cb1f6b066f4bea51481
Hyesung Park <jayhspark@gmail.com>
2021-09-09 15:48:06 +0900

    update

--- level1/three.js
+++ level1/three.js
@@ -1,7 +1,16 @@
 function solution(n) {
-  console.log(n);
-  var answer = 0;
+  const conver = n.toString(3);
+  let arr = conver.split("");
+  let arr2 = conver.split("");
+  let arrLength = Math.floor(arr.length / 2);
+
+  for (let i = 0; i < arrLength; i++) {
+    arr[arr.length - 1 - i] = arr2[i];
+    arr[i] = arr2[arr.length - 1 - i];
+  }
+  const answer = parseInt(arr.join(""), 3);
+  console.log(answer);
   return answer;
 }
 
-solution(45);
+solution(78413450);
