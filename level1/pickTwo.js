793f7d02d111ac4f6ae95757e8222d289a2c73a9
Hyesung Park <jayhspark@gmail.com>
2021-09-09 10:24:51 +0900

    update

--- level1/pickTwo.js
+++ level1/pickTwo.js
@@ -1,15 +1,64 @@
+// function solution(numbers) {
+//   let mock = [];
+//   let realMock = [];
+
+//   for (let i = 0; i < numbers.length; i++) {
+//     for (let j = 0; j < numbers.length; j++) {
+//       let chk = true;
+//       for (let k = 0; k < mock.length; k++) {
+//         if (JSON.stringify(mock[k]) === JSON.stringify([i, j].sort())) {
+//           chk = false;
+//         }
+//       }
+//       if (i === j) {
+//         chk = false;
+//       }
+//       if (chk) {
+//         mock.push([i, j].sort());
+//         realMock.push(numbers[i] + numbers[j]);
+//       }
+//     }
+//   }
+//   const answer = Array.from(new Set(realMock));
+
+//   // let answer = [];
+//   // for (let i = 0; i < realMock.length; i++) {
+//   //   let chk = true;
+//   //   for (let j = 0; j < answer.length; j++) {
+//   //     if (JSON.stringify(realMock[i]) === JSON.stringify(answer[j])) {
+//   //       chk = false;
+//   //     }
+//   //   }
+//   //   if (chk) {
+//   //     answer.push(realMock[i]);
+//   //   }
+//   // }
+
+//   // console.log(set);
+//   const sortByAsc = (a, b) => {
+//     return a - b;
+//   };
+//   answer.sort(sortByAsc);
+
+//   return answer;
+// }
+
 function solution(numbers) {
-  let mock = [];
-  // const set = new Set();
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = 0; j < numbers.length; j++) {
-      console.log(i.toString(), j.toString());
-      // set.add({ [i]: j });
+  const result = [];
+
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      const sum = numbers[i] + numbers[j];
+      // console.log(sum);
+      if (result.indexOf(sum) < 0) {
+        result.push(sum);
+      }
     }
   }
-  // console.log(set);
-  var answer = [];
-  return answer;
+
+  result.sort((a, b) => a - b);
+  return result;
 }
 
-solution([2, 1, 3, 4, 1]);
+const value = solution([5, 0, 2, 7]);
+console.log(value);
