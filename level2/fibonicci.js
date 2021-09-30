8ed6b61b2cf16b76c013c0db07d49b0b98aaf5a9
박혜성 <nelv13@openbase.co.kr>
2021-09-30 16:59:32 +0900

    update

--- level2/fibonicci.js
+++ level2/fibonicci.js
@@ -1,7 +1,20 @@
 function solution(n) {
+  let mock = [];
+  let vla = 0;
   for (let i = 0; i < n; i++) {
+    if (i < 2) {
+      vla += i;
+    }
+    const value = {};
+
     console.log(i);
+    // console.log(i);
+    // let val = (i-1) + (i - 2);
+    // mock.push(val);
   }
+  console.log(mock, vla);
   var answer = 0;
   return answer;
 }
+
+solution(5);
