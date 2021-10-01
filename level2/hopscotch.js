2b036f39a380cbdb62c4a0800570df138600fa0a
박혜성 <nelv13@openbase.co.kr>
2021-10-01 16:57:08 +0900

    update

--- level2/hopscotch.js
+++ level2/hopscotch.js
@@ -1,31 +1,76 @@
+// function solution(land) {
+//   let maxValue = [0, 0, 0];
+//   for (let i = 0; i < land.length; i++) {
+//     const value = land[i].reduce(
+//       (acc, cur, index) => {
+//         if (acc[0] < cur) {
+//           acc[0] = cur;
+//           acc[1] = index;
+//           acc[2] = i;
+//         }
+//         return acc;
+//       },
+//       [0, 0, 0]
+//     );
+
+//     maxValue = maxValue[0] * 2 <= value[0] ? value : maxValue;
+//     console.log(value);
+//   }
+//   console.log(maxValue);
+//   let answer = 0;
+
+//   return answer;
+// }
+
+// const value = solution([
+//   [1, 2, 3, 5],
+//   [5, 6, 7, 10],
+//   [4, 3, 2, 20],
+
+// console.log(value);
+
 function solution(land) {
-  let maxValue = [0, 0, 0];
-  for (let i = 0; i < land.length; i++) {
-    const value = land[i].reduce(
+  let wholeLength = land.length - 2;
+  const maxValue = land[0].reduce(
+    (acc, cur, index) => {
+      if (acc[0] < cur) {
+        acc[0] = cur;
+        acc[1] = index;
+      }
+      return acc;
+    },
+    [land[0][0], 0]
+  );
+  let index = 0;
+  const tailRecursive = (choosen, leng, nextArr) => {
+    const result = nextArr.reduce(
       (acc, cur, index) => {
-        if (acc[0] < cur) {
+        if (acc[0] < cur && choosen[1] !== index) {
           acc[0] = cur;
           acc[1] = index;
-          acc[2] = i;
         }
         return acc;
       },
-      [0, 0, 0]
+      [nextArr[0], 0]
     );
+    console.log("this is choosen", choosen, nextArr, result);
+    index++;
+  };
 
-    maxValue = maxValue[0] * 2 <= value[0] ? value : maxValue;
-    console.log(value);
-  }
-  console.log(maxValue);
-  let answer = 0;
+  const slicedArr = land.slice(1, 2).flat();
+  tailRecursive(maxValue, wholeLength, slicedArr);
 
+  // slicedArr.map((element, index) => {
+  //   console.log(element);
+  // });
+  console.log(maxValue, slicedArr);
+
+  let answer = "";
   return answer;
 }
 
-const value = solution([
+solution([
   [1, 2, 3, 5],
-  [5, 6, 7, 10],
-  [4, 3, 2, 20],
+  [5, 6, 7, 8],
+  [4, 3, 2, 1],
 ]);
-
-// console.log(value);
