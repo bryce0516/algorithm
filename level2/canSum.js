f06e43268edf95903bffdbcd961e30ea445c19ee
Hyesung Park <jayhspark@gmail.com>
2021-10-05 22:43:23 +0900

    update

--- level2/canSum.js
+++ level2/canSum.js
@@ -1,21 +1,52 @@
-const canSum = (targetSum, numbersm, memo = {}) => {
+// const canSum = (targetSum, numbers, memo = {}) => {
+//   if (targetSum in memo) return memo[targetSum];
+//   if (targetSum === 0) return true;
+//   if (targetSum < 0) return false;
+//   for (let num of numbers) {
+//     const remainder = targetSum - num;
+
+//     if (canSum(remainder, numbers, memo) === true) {
+//       memo[targetSum] = true;
+//       return true;
+//     }
+//   }
+//   memo[targetSum] = false;
+//   return false;
+// };
+
+
+const howSum = (targetSum, numbers, memo = {}) => {
   if (targetSum in memo) return memo[targetSum];
-  if (targetSum === 0) return true;
-  if (targetSum < 0) return false;
+  if (targetSum === 0) return []
+  if (targetSum < 0) return null
+
   for (let num of numbers) {
     const remainder = targetSum - num;
+    const remainderResult = howSum(remainder, numbers, memo)
 
-    if (canSum(remainder, numbers, memo) === true) {
-      memo[targetSum] = true;
-      return true;
+    if(remainderResult !== null) {
+      memo[targetSum] = [...remainderResult, num]
+      return memo[targetSum]
     }
+
   }
-  memo[targetSum] = false;
-  return false;
+  memo[targetSum] = null
+  return null
 };
 
-console.log(canSum(7, [2, 3]));
-console.log(canSum(7, [5, 3, 4, 7]));
-console.log(canSum(7, [2, 4]));
-console.log(canSum(8, [2, 3, 5]));
-console.log(canSum(300, [7, 14]));
+
+
+console.log(howSum(7, [2, 3]));
+console.log(howSum(7, [5, 3, 4, 7]));
+console.log(howSum(7, [2, 4]));
+console.log(howSum(8, [2, 3, 5]));
+console.log(howSum(300, [7, 14]));
+
+
+
+
+// console.log(canSum(7, [2, 3]));
+// console.log(canSum(7, [5, 3, 4, 7]));
+// console.log(canSum(7, [2, 4]));
+// console.log(canSum(8, [2, 3, 5]));
+// console.log(canSum(300, [7, 14]));
