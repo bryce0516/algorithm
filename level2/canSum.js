027b7a760caa8ec759cc7befce0c356701d21e97
Hyesung Park <jayhspark@gmail.com>
2021-10-05 23:44:47 +0900

    update

--- level2/canSum.js
+++ level2/canSum.js
@@ -15,32 +15,60 @@
 // };
 
 
-const howSum = (targetSum, numbers, memo = {}) => {
-  if (targetSum in memo) return memo[targetSum];
+// const howSum = (targetSum, numbers, memo = {}) => {
+//   if (targetSum in memo) return memo[targetSum];
+//   if (targetSum === 0) return []
+//   if (targetSum < 0) return null
+
+//   for (let num of numbers) {
+//     const remainder = targetSum - num;
+//     const remainderResult = howSum(remainder, numbers, memo)
+//     console.log(num)
+//     console.log("reminderResult", remainderResult)
+//     if(remainderResult !== null) {
+//       memo[targetSum] = [...remainderResult, num]
+//       return memo[targetSum]
+//     }
+
+//   }
+//   memo[targetSum] = null
+//   return null
+// };
+
+const bestSum = (targetSum, numbers) => {
   if (targetSum === 0) return []
   if (targetSum < 0) return null
 
+  let shortestCombination = null
+
   for (let num of numbers) {
-    const remainder = targetSum - num;
-    const remainderResult = howSum(remainder, numbers, memo)
+    const remainder = targetSum - num
+    const remainderCombination = bestSum(remainder, numbers)
+    if (shortestCombination !== null) {
+      const combination = [...remainderCombination, num]
 
-    if(remainderResult !== null) {
-      memo[targetSum] = [...remainderResult, num]
-      return memo[targetSum]
+      if (shortestCombination === null || combination.length < shortestCombination.length) {
+        shortestCombination = combination
+      }
     }
-
   }
-  memo[targetSum] = null
-  return null
-};
+
+  return shortestCombination
+}
+
+console.log(bestSum(7, [5, 3, 4, 7]));
+// console.log(bestSum(8, [2, 3, 5]));
+// console.log(bestSum(8, [1, 4, 5]));
+// console.log(bestSum(100, [1, 2, 5, 25]));
 
 
 
-console.log(howSum(7, [2, 3]));
-console.log(howSum(7, [5, 3, 4, 7]));
-console.log(howSum(7, [2, 4]));
-console.log(howSum(8, [2, 3, 5]));
-console.log(howSum(300, [7, 14]));
+
+// console.log(howSum(7, [2, 3]));
+// console.log(howSum(7, [5, 3, 4, 7]));
+// console.log(howSum(7, [2, 4]));
+// console.log(howSum(8, [2, 3, 5]));
+// console.log(howSum(300, [7, 14]));
 
 
 
