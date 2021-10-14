0256b8420770281cf179ad932cf8aa89488dd653
bryce0516 <jayhspark@gmail.com>
2021-10-14 23:14:26 +0900

    update

--- level2/properWriting.js
+++ level2/properWriting.js
@@ -1,12 +1,22 @@
 function solution(s) {
-  const target = s.split("");
-  const first = target.filter((element) => element === "(");
-  const second = target.filter((element) => element === ")");
-  if (first.length === second.length) {
-    return true;
-  } else {
-    return false;
+  const val = s.match(/()/g);
+
+  class RegExp1 extends RegExp {
+    [Symbol.match](str) {
+      const result = RegExp.prototype[Symbol.match].call(this, str);
+      console.log("this is result", result);
+      return result.map((x) => `(${x})`);
+    }
   }
+
+  console.log(s.match(new RegExp1("()")));
+  // const first = target.filter((element) => element === "(");
+  // const second = target.filter((element) => element === ")");
+  // if (first.length === second.length) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 }
 
 solution("()()");
