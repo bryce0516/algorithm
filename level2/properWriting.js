ff9e3606cea84113c9e78a5301b7645eb61bb917
Hyesung Park <jayhspark@gmail.com>
2021-10-18 16:53:21 +0900

    update

--- level2/properWriting.js
+++ level2/properWriting.js
@@ -1,22 +1,63 @@
 function solution(s) {
-  const val = s.match(/()/g);
+  const arr = s.split("");
+  // const recursion = (arr, start, YN) => {
+  //   console.log("this is recursion", start, arr);
+  //   if (start === s.length) {
+  //     return;
+  //   }
+  //   if (arr.length === 0) {
+  //     return true;
+  //   }
 
-  class RegExp1 extends RegExp {
-    [Symbol.match](str) {
-      const result = RegExp.prototype[Symbol.match].call(this, str);
-      console.log("this is result", result);
-      return result.map((x) => `(${x})`);
+  //   let index = 0;
+  //   while (true) {
+  //     if (arr[index] === "(" && arr[index + 1] === ")") {
+  //       if (index === 0) {
+  //         arr.splice(0, 2);
+  //       } else {
+  //         arr.splice(index, index + 1);
+  //       }
+  //       console.log("in while", start, index, "result ======>", arr);
+  //     } else {
+  //       YN = false;
+  //     }
+  //     console.log(index);
+  //     index++;
+  //     if (index === arr.length) {
+  //       break;
+  //     }
+  //   }
+
+  //   return recursion(arr, start + 1, YN);
+  // };
+
+  // const answer = recursion(arr, 0);
+  // console.log(answer);
+
+  const recursion = (array, start) => {
+    if (start === s.length) {
+      return console.log("end recursion");
     }
-  }
+    console.log("index num", start);
+    let index = 0;
+    while (true) {
+      index++;
+      console.log("in while =====>  ", index);
+      if (array[index] === "(" && array[index + 1] === ")") {
+        if (index === 0) {
+          array.splice(0, 2);
+        } else {
+          array.splice(index, index + 1);
+        }
+      }
 
-  console.log(s.match(new RegExp1("()")));
-  // const first = target.filter((element) => element === "(");
-  // const second = target.filter((element) => element === ")");
-  // if (first.length === second.length) {
-  //   return true;
-  // } else {
-  //   return false;
-  // }
+      if (index === array.length) {
+        break;
+      }
+    }
+    return recursion(array, start + 1);
+  };
+  recursion(arr, 0);
 }
 
-solution("()()");
+solution("(())()");
