98f39241eac681e7696694f75886fb7d83fa6396
Hyesung Park <jayhspark@gmail.com>
2021-10-18 14:24:32 +0900

    update

--- level2/nGame.js
+++ level2/nGame.js
@@ -1,22 +1,91 @@
 function solution(n, t, m, p) {
-  var answer = "";
-  const num = t.toString(2);
-  const target = Array.from({ length: t + 1 }, (_, i) => i)
+  const mkArr = Array.from({ length: t * m }, (_, i) =>
+    i.toString(n).toUpperCase()
+  )
     .join("")
-    .split("");
+    .split("")
+    .filter((element2, index2) => index2 % m === p - 1)
+    .join("")
+    .slice(0, t);
 
-  target.map((element) => {
-    const value = Number(element).toString(2);
-    console.log(value);
-  });
-  // for (let i = 0; i < t; i++) {
-  //   const element = i.toString(n);
-  //   // if (i % 2 === 0) {
-  //   //   console.log(element, i);
-  //   // }
-  //   console.log(element, i);
-  // }
-  return answer;
+  return mkArr;
 }
 
-solution(2, 11, 2, 1);
+// while (true) {
+//   const change = index.toString(n);
+//   pushWhole.push(change);
+//   const target = pushWhole
+//     .join("")
+//     .split("")
+//     .filter((element, index2) => index2 % m === p - 1)
+//     .map((element3, index3) => {
+//       // console.log("this is element === >", element3, index3);
+//       return element3;
+//     });
+//   answer = target;
+//   // console.log("this is target ===> ", target);
+//   if (target.length >= t) {
+//     break;
+//   }
+//   index++;
+// }
+// const answer2 = answer.map((element) => element.toUpperCase()).join("");
+// .slice(0, t);
+// console.log("this is answer2", answer2);
+// return answer2;
+// }
+
+// function solution(n, t, m, p) {
+//   let index = 0;
+//   let pushWhole = [];
+//   let answer = [];
+//   let memo = {};
+//   while (true) {
+//     const change = index.toString(n).toUpperCase();
+//     console.log("this is change==>", change);
+//     pushWhole.push(change);
+//     const target = pushWhole
+//       .join("")
+//       .split("")
+//       .filter((element, index2) => index2 % m === p - 1)
+//       .join("")
+//       .slice(0, t);
+//     answer = target;
+
+//     if (target.length >= t) {
+//       break;
+//     }
+//     index++;
+//   }
+//   const answer2 = answer;
+//   return answer2;
+// }
+
+const t = solution(2, 4, 2, 1);
+console.log(t);
+
+// function solution(n, t, m, p) {
+//   let index = 0;
+//   let pushWhole = [];
+//   let answer = [];
+//   let memo = {};
+//   while (true) {
+//     const change = index.toString(n).toUpperCase();
+//     console.log("this is change==>", change);
+//     pushWhole.push(change);
+//     const target = pushWhole
+//       .join("")
+//       .split("")
+//       .filter((element, index2) => index2 % m === p - 1)
+//       .join("")
+//       .slice(0, t);
+//     answer = target;
+
+//     if (target.length >= t) {
+//       break;
+//     }
+//     index++;
+//   }
+//   const answer2 = answer;
+//   return answer2;
+// }
