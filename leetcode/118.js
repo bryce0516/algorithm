f14336a3a8d63de248d4af4cea49164e2d12cf8d
Hyesung Park <jayhspark@gmail.com>
2021-11-22 13:13:59 +0900

    update

--- leetcode/118.js
+++ leetcode/118.js
@@ -1,27 +1,68 @@
+//success but faster than 5.09
+// var generate = function (numRows) {
+//   if (numRows === 1) return [[1]];
+//   if (numRows === 2) return [[1], [1, 1]];
+//   let arr = [[1], [1, 1]];
+//   for (let i = 1; i < numRows - 1; i++) {
+//     let target = [];
+//     let mock = [];
+//     let value = 0;
+//     for (let j = 0; j < arr[arr.length - 1].length; j++) {
+//       value += arr[i][j];
+//       mock.push(value);
+//       value = arr[i][j];
+//     }
+
+//     target = [...mock, 1];
+//     arr = [...arr, [...target]];
+//   }
+//   console.log(arr);
+
+//   return arr;
+// };
+
 var generate = function (numRows) {
-  let first = 0;
-  let second = 1;
+  if (numRows === 1) return [[1]];
   let arr = [[1], [1, 1]];
-  for (let i = 0; i < numRows - arr.length; i++) {
-    let target = [];
-    let mock = console.log(">>>>>>>>>>>>>>>>>>> index", i);
-    for (let j = 0; j < arr[arr.length - 1].length; j++) {
-      mock += arr[i][j];
+  for (let i = 1; i < numRows - 1; i++) {
+    let target = arr[i];
+    let value = 0;
+    let mock = [];
+    console.log("target >>> ", target);
+    for (let j = 0; j < target.length; j++) {
+      console.log(i, j, ">>>> ");
+      value += arr[i][j];
+      mock.push(value);
+      value = arr[i][j];
     }
     console.log(mock);
-    target = [1, ...mock, 1];
+    target = [...mock, 1];
     arr = [...arr, [...target]];
   }
   console.log(arr);
-  // const recursive = (start, end, memo) => {
-  //   if (start === end + 1) return memo;
 
-  //   console.log(start, memo);
-
-  //   return (memo =
-  //     recursive(start + 1, end, memo) + recursive(start + 1, end, memo));
-  // };
-  // const answer = recursive(0, numRows, []);
-  // console.log(answer);
+  return arr;
 };
-const value = generate(5);
+
+// var generate = function (numRows) {
+//   if (numRows === 1) return [[1]];
+//   let i;
+//   let j;
+//   let row = [1, 1];
+//   let res = [];
+//   let temp = [];
+//   for (i = 1; i <= numRows; i++) {
+//     temp = Array(i).fill(1);
+//     console.log("temp", temp);
+//     for (j = 0; j < i; j++) {
+//       if (j !== 0 && j !== i - 1) {
+//         temp[j] = row[j - 1] + row[j];
+//         console.log(i, j);
+//       }
+//     }
+//     res.push(temp);
+//     row = temp;
+//   }
+//   console.log(res);
+// };
+const value = generate(4);
