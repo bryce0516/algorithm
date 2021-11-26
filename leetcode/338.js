6f735a4eb8bc76c40b990beb2af56bff58e56e62
Hyesung Park <jayhspark@gmail.com>
2021-11-26 17:43:06 +0900

    update

--- leetcode/338.js
+++ leetcode/338.js
@@ -1,6 +1,28 @@
-var countBits = function (n) {
-  for (let i = 0; i < n; i++) {
-    console.log(i);
-  }
-};
-countBits(2);
+// stackoverflow
+// var countBits = function (n) {
+//   const initialValue = Math.floor(Math.sqrt(n + 1));
+//   console.log("this is initial", initialValue);
+
+//   const memo = { 0: [1] };
+//   const makeArray = (res, memo) => {
+//     if (memo[res]) {
+//       return memo[res];
+//     }
+//     const before = memo[res - 1];
+//     const after = memo[res - 1].map((element) => element + 1);
+//     memo[res] = [...before, ...after];
+//   };
+
+//   for (let i = 0; i <= initialValue; i++) {
+//     makeArray(i, memo);
+//   }
+
+//   console.log(memo);
+//   let answer = [0];
+//   for (const [key, value] of Object.entries(memo)) {
+//     answer = [...answer, ...value];
+//   }
+//   const re = answer.slice(0, n + 1);
+//   return re
+// };
+countBits(5);
