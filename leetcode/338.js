412ecac3fb939af38bde5801ad562bea05054201
Hyesung Park <jayhspark@gmail.com>
2021-11-30 12:33:23 +0900

    update

--- leetcode/338.js
+++ leetcode/338.js
@@ -23,22 +23,45 @@
 //     answer = [...answer, ...value];
 //   }
 //   const re = answer.slice(0, n + 1);
-//   return re
+//   return re;
 // };
+//heap memory overflow
+// var countBits = function (n) {
+//   const initialValue = Math.floor(Math.sqrt(n + 1));
+//   console.log("initialValue,", initialValue);
+//   let before = [1];
+//   let current = [1];
+//   for (let i = 0; i <= initialValue; i++) {
+//     const targetPlus = before.map((element) => element + 1);
+//     const target = [...before, ...targetPlus];
+//     before = target;
+//     current = [...current, ...target];
+//     // current = [...before, target];
+//     // console.log(i, before, current);
+//   }
+//   current = [0, ...current];
+//   const answer = current.slice(0, n + 1);
+//   return answer;
+// };
+// var countBits = function (n) {
+//   const makeArray = Array.from({ length: n + 1 }, (_, i) => i);
+//   for (let x = 1; x <= n; ++x) {
+//     makeArray[x] = makeArray[x & (x - 1)] + 1;
+//   }
+//   return makeArray
+
+// };
+
 var countBits = function (n) {
-  const initialValue = Math.floor(Math.sqrt(n + 1));
-  console.log("this is initial", initialValue);
+  let ans = new Array(n + 1);
+  ans[0] = 0;
+  let offset = 1;
 
-  const before = [1];
-  const current = [1];
-
-  for (let i = 0; i <= initialValue; i++) {
-    const target = before.map((element) => element + 1);
-
-    current = [...before, target];
-    console.log(target, current);
-    console.log(i);
+  for (let i = 1; i <= n; i++) {
+    if (offset * 2 === i) offset = i;
+    ans[i] = 1 + ans[i - offset];
   }
+  return ans;
 };
 
-countBits(2);
+countBits(5);
