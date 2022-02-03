f5237ada968ea28d8b727feda23db17095a628d0
Hyesung Park <jayhspark@gmail.com>
2022-02-03 12:18:32 +0900

    update

--- coltSteele/countUniqueValues.js
+++ coltSteele/countUniqueValues.js
@@ -25,7 +25,7 @@ const result = countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]);
 console.log("this is result ", result.length);
 
 // const countUniqueValues = function (arr) {
-
+//if (arr.length === 0) return 0;
 //   let i = 0;
 //   for (let j = 1; j < arr.length; j++) {
 //     if (arr[i] !== arr[j]) {
