660c5d257367ef0e0c1c2a007ded6f5c65500b0d
Hyesung Park <jayhspark@gmail.com>
2022-02-08 12:26:04 +0900

    update

---
+++ coltSteele/findLongestSubstring.js
@@ -0,0 +1,34 @@
+// const findLongestSubstring = function(str) {
+//   console.log(str, str.length, str[0])
+  
+//   let maxLen = -Infinity
+//   let index= 0
+//   let checkIndex = 1
+//   let checka = []
+//   while (index < str.length) {
+
+//     if(str[checkIndex] !== str[index] && str[checkIndex] ) {
+//       console.log('first',str[checkIndex],str[index], index, checkIndex)
+//       checkIndex++
+//     } else if(str[checkIndex] === str[index]){
+//       console.log('second',str[checkIndex],str[index])
+//       console.log('checkIndex - index',checkIndex - index)
+//       checka.push([str[index], str[checkIndex],checkIndex - index])
+//       maxLen = Math.max(maxLen, checkIndex - index)
+//       index++
+//       checkIndex = index +1 
+//     } else if(checkIndex === str.length){
+//       index++
+//       checkIndex = index +1 
+//     } 
+//   }
+//   console.log('this is min len',maxLen,checka)
+
+// }
+
+const findLongestSubstring = function(str) {
+ let /l
+
+}
+
+const result = findLongestSubstring('rithmschool')
