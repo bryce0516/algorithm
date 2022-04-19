7cd0c7607c787469e1a0b97cf532360b6145972c
Hyesung Park <jayhspark@gmail.com>
2022-04-19 10:51:31 +0900

    update

--- coltSteele/bubble.js
+++ coltSteele/bubble.js
@@ -27,17 +27,37 @@
 //   return arr;
 // }
 
+// function bubbleSort(arr) {
+//   const swap = (arr, idx1, idx2) => {
+//     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+//   };
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       console.log(arr, arr[j], arr[j + 1]);
+//       if (arr[j] > arr[j + 1]) {
+//         swap(arr, j, j + 1);
+//       }
+//     }
+//     console.log("on pass!!!!!!!!!!!!!!!!!!!!!!!");
+//   }
+//   return arr;
+// }
+
+//optimize
 function bubbleSort(arr) {
-  const swap = (arr, idx1, idx2) => {
-    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-  };
+  let noSwaps;
   for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
       console.log(arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
-        swap(arr, j, j + 1);
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
     console.log("on pass!!!!!!!!!!!!!!!!!!!!!!!");
   }
   return arr;
