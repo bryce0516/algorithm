fbcb7a2206b8b4bf4c897f36cd6b23d5f401e51a
Hyesung Park <jayhspark@gmail.com>
2022-04-19 10:22:09 +0900

    update

---
+++ coltSteele/bubble.js
@@ -0,0 +1,47 @@
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       console.log(arr, arr[j], arr[j + 1]);
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// function bubbleSort(arr) {
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       console.log(arr, arr[j], arr[j + 1]);
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//     console.log("on pass!!!!!!!!!!!!!!!!!!!!!!!");
+//   }
+//   return arr;
+// }
+
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+    console.log("on pass!!!!!!!!!!!!!!!!!!!!!!!");
+  }
+  return arr;
+}
+
+const value = bubbleSort([37, 45, 29, 8, 12, 88, 100]);
+// console.log(value);
