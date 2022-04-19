b08b77ac862247af169c96aee5bd5f028ad6de47
Hyesung Park <jayhspark@gmail.com>
2022-04-19 15:51:47 +0900

    update

---
+++ leetcode/278.js
@@ -0,0 +1,10 @@
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {};
+};
+
+const value = solution();
+console.log(value);
