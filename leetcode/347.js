d70a729f3b14542a1729c542593d34a61c5ac4d2
Hyesung Park <51251258+bryce0516@users.noreply.github.com>
2022-08-29 16:24:36 +0900

    update

--- leetcode/347.js
+++ leetcode/347.js
@@ -1,11 +1,6 @@
 var topKFrequent = function (nums, k) {
-  let empty = [];
   if (nums.length === k) return nums;
-  const set = new Set(nums);
-
-  const arr = [...set];
-  console.log("nums", nums);
-
+  const empty = [];
   const reduced = nums.reduce((acc, cur, index) => {
     // console.log("==>", index, acc, cur);
     if (Object.hasOwn(acc, cur)) {
@@ -16,15 +11,32 @@ var topKFrequent = function (nums, k) {
       return acc;
     }
   }, {});
+  const sorted = Object.entries(reduced).sort((a, b) => b[1] - a[1]);
+  for (let i = 0; i < k; i++) {
+    empty.push(Number(sorted[i][0]));
+  }
 
-  console.log("<reduced", reduced);
-  let decision = -Infinity;
-
-  const sorted = Object.entries(reduced)
-    .sort((a, b) => b[1] - a[1])
-    .map((element) => Number(element[0]))
-    .slice(0, k);
-  return sorted;
+  return empty;
 };
+
+// var topKFrequent = function (nums, k) {
+//   const numsCount = {};
+//   const res = [];
+//   for (let num of nums) {
+//     numsCount[num] = (numsCount[num] ?? 0) + 1;
+//   }
+//   let bucket = Array.from({ length: nums.length + 1 }, () => []);
+//   for (let num in numsCount) {
+//     bucket[numsCount[num]].push(num);
+//   }
+
+//   for (let i = bucket.length - 1; i >= 0; i--) {
+//     if (bucket[i].length > 0) {
+//       bucket[i].forEach((elem) => res.push(elem));
+//       if (res.length === k) return res;
+//     }
+//   }
+//   console.log(bucket);
+// };
 
 topKFrequent([2, 3, 4, 1, 4, 0, 4, -1, -2, -1], 2);
