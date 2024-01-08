7b834975a3e18557dff72eadd12e598185c9f225
bryce0516 <jayhspark@gmail.com>
2024-01-08 17:26:09 +0900

    update

--- level1/2024winterinternship1.js
+++ level1/2024winterinternship1.js
@@ -1,5 +1,4 @@
 function solution(friends, gifts) {
-  let chart = []
   let givers = Array.from({ length: friends.length }, (_, i) => {
     let copy = [...friends]
     copy.splice(i, 1)
@@ -7,23 +6,35 @@ function solution(friends, gifts) {
     copy.map((element, index) => {
       obj[element] = 0
     })
-    chart.push(friends[i], { give: 0, receive: 0, point: 0 })
-    _ = [friends[i], obj]
+    _ = [friends[i], obj, 0, 0, 0]
+    // 이름 / 전적 / 준개수 / 받은개수 / 점수
     return _
-  }).map(([giver, target], index) => {
-    gifts.map((element, index) => {
+  }).map(([giver, target, ...rest], index) => {
+    gifts.map((element, index2) => {
+      // from 준사람 to 받은사람
       const [from, to] = element.split(" ")
+      // 받은경우
       if (from === giver) {
         target[to] += 1
-        console.log(giver, to)
-      }
-    })
-    return [giver, target]
-  }).map(([giver, target], index) => {
-    Object.values(target).map((element, index) => {
+        rest[0] += 1
 
+      } else if (to === giver) {
+        rest[1] += 1
+      }
+      // console.log(giver, to, rest)
     })
+
+
+
+    // rest[2] = rest[0] - rest[1]
+    // console.log("final", [giver, target, ...rest])
+    return [giver, target, rest[0], rest[1], rest[2] = rest[0] - rest[1], rest[3]]
   })
+  // .map(([giver, target], index) => {
+  //   Object.values(target).map((element, index) => {
+
+  //   })
+  // })
   console.log(givers)
 
 
