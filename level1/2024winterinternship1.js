5775278baaa9f7ac3ac64b32fcab2a43e545f39d
bryce0516 <jayhspark@gmail.com>
2024-01-09 09:59:42 +0900

    UPDATE

--- level1/2024winterinternship1.js
+++ level1/2024winterinternship1.js
@@ -1,59 +1,95 @@
 function solution(friends, gifts) {
-  let givers = Array.from({ length: friends.length }, (_, i) => {
-    let copy = [...friends]
-    copy.splice(i, 1)
-    const obj = {}
-    copy.map((element, index) => {
-      obj[element] = 0
-    })
-    _ = [friends[i], obj, 0, 0, 0]
-    // 이름 / 전적 / 준개수 / 받은개수 / 점수
-    return _
-  }).map(([giver, target, ...rest], index) => {
-    gifts.map((element, index2) => {
-      // from 준사람 to 받은사람
-      const [from, to] = element.split(" ")
-      // 받은경우
-      if (from === giver) {
-        target[to] += 1
-        rest[0] += 1
 
-      } else if (to === giver) {
-        rest[1] += 1
-      }
-      // console.log(giver, to, rest)
-    })
-
-
-
-    // rest[2] = rest[0] - rest[1]
-    // console.log("final", [giver, target, ...rest])
-    return [giver, target, rest[0], rest[1], rest[2] = rest[0] - rest[1], rest[3]]
+  let scoreMap = new Object()
+  friends.map((element, index) => {
+    scoreMap[element] = index
   })
-  // .map(([giver, target], index) => {
-  //   Object.values(target).map((element, index) => {
+  let scoreTable = Array.from({ length: friends.length }, (_) => Array(friends.length).fill(0))
+  let pointMap = new Array(friends.length).fill(0)
+  let result = new Array(friends.length).fill(0)
+  gifts.map((element, index) => {
+    const [from, to] = element.split(" ")
+    const fromIndex = scoreMap[from]
+    const toIndex = scoreMap[to]
+    scoreTable[fromIndex][toIndex] += 1
+  })
 
-  //   })
-  // })
-  console.log(givers)
+  for (let i = 0; i < friends.length; i++) {
+    for (let j = 0; j < friends.length; j++) {
+      pointMap[i] += scoreTable[i][j]
+      pointMap[i] -= scoreTable[j][i]
+    }
+  }
+  console.log("scoreTable", scoreTable)
+  console.log("pointMap", pointMap)
+  for (let i = 0; i < friends.length; i++) {
+    for (let j = i + 1; j < friends.length; j++) {
+      if (i === j) continue;
+      if (scoreTable[i][j] > scoreTable[j][i]) { result[i]++ }
+      if (scoreTable[i][j] < scoreTable[j][i]) { result[j]++ }
+      if (scoreTable[i][j] === scoreTable[j][i]) {
+        if (pointMap[i] > pointMap[j]) { result[i]++ }
+        if (pointMap[i] < pointMap[j]) { result[j]++ }
+      }
+    }
+  }
+  console.log("result", result)
 
-
-
-
-  var answer = 0;
-  return answer;
+  return Math.max(...result)
 }
 
-const friends = ["muzi", "ryan", "frodo", "neo"]
-const gifts = [
-  "muzi frodo",
-  "muzi frodo",
-  "ryan muzi",
-  "ryan muzi",
-  "ryan muzi",
-  "frodo muzi",
-  "frodo ryan",
-  "neo muzi"
-]
 
-solution(friends, gifts)
+const friends = ["joy", "brad", "alessandro", "conan", "david"]
+const gifts = ["alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"]
+
+const answer = solution(friends, gifts)
+console.log("this is answer", answer)
+
+// function solution(friends, gifts) {
+//   const len = friends.length;
+//   const nameMap = new Map();
+//   const giftTable = new Array(len).fill(0).map(_ => new Array(len).fill(0));
+//   const rankInfo = new Array(len).fill(0);
+//   const nextMonth = new Array(len).fill(0);
+
+//   // 친구들 키 값 저장.
+//   friends.forEach((name, idx) => {
+//     nameMap.set(name, idx);
+//   })
+
+
+//   // 선물 주고받은 내역 기록
+//   gifts.forEach(info => {
+//     const [from, to] = info.split(" ");
+//     giftTable[nameMap.get(from)][nameMap.get(to)]++;
+//   })
+
+//   console.log(nameMap)
+//   console.log("giftTable", giftTable)
+//   // 기록을 바탕으로 선물 지수 계산
+//   for (let i = 0; i < len; i++) {
+//     rankInfo[i] = giftTable[i].reduce((acc, cur) => acc += cur, 0)
+
+//     for (let j = 0; j < len; j++) {
+//       rankInfo[i] -= giftTable[j][i];
+//     }
+//   }
+
+//   console.log(rankInfo)
+//   // 다음 달 받을 선물 계산
+//   for (let i = 0; i < len; i++) {
+//     for (let j = i + 1; j < len; j++) {
+//       if (i === j) continue;
+//       if (giftTable[i][j] > giftTable[j][i]) nextMonth[i]++;
+//       if (giftTable[i][j] < giftTable[j][i]) nextMonth[j]++;
+//       if (giftTable[i][j] === giftTable[j][i]) {
+//         if (rankInfo[i] > rankInfo[j]) nextMonth[i]++
+//         if (rankInfo[i] < rankInfo[j]) nextMonth[j]++
+//       }
+//     }
+//   }
+//   console.log(nextMonth)
+
+//   // 최댓값 반환
+//   return Math.max(...nextMonth)
+// }
