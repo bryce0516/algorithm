6eeebb12e805673afcd7c5db6dc1ce2020734dcd
bryce0516 <jayhspark@gmail.com>
2024-01-12 11:36:48 +0900

    finish

--- level1/race.js
+++ level1/race.js
@@ -1,32 +1,54 @@
 function solution(players, callings) {
+  const hashPlayer = {}
+  const hashRank = {}
+  for (const element in players) {
+    hashPlayer[players[element]] = Number(element)
+    hashRank[element] = players[element]
+  }
+
   let _index = 0
   let endIndex = callings.length
-  const recursion = (caller, origin) => {
-    if (_index === endIndex) {
-      return origin
-    }
-    let end = true
-    let $index = 0
-    while (end) {
-      if ($index === origin.length - 1) {
-        end = false
-      }
-      if (caller === origin[$index]) {
+  while (_index < endIndex) {
+    const target = callings[_index]
+    const targetIndex = Number(hashPlayer[target])
+    const after = hashRank[targetIndex - 1]
+    const afterIndex = targetIndex - 1
 
-        if ($index >= 1) {
-          let target = origin[$index]
-          let after = origin[$index - 1]
-          origin.splice($index, 1, after)
-          origin.splice($index - 1, 1, target)
-          end = false
-        }
-      }
-      $index++
-    }
+    hashPlayer[target] = afterIndex
+    hashPlayer[after] = targetIndex
+    hashRank[targetIndex] = after
+    hashRank[afterIndex] = target
     _index++
-    return recursion(callings[_index], origin)
   }
-  const dd = recursion(callings[_index], players)
+
+  return Object.values(hashRank)
+  // const recursion = (caller, origin) => {
+  //   if (_index === endIndex) {
+  //     return origin
+  //   }
+  //   let end = true
+  //   let $index = 0
+  //   console.log(caller, origin)
+  //   while (end) {
+  //     if ($index === origin.length - 1) {
+  //       end = false
+  //     }
+  //     if (caller === origin[$index]) {
+
+  //       if ($index >= 1) {
+  //         // let target = origin[$index]
+  //         // let after = origin[$index - 1]
+  //         // origin.splice($index, 1, after)
+  //         // origin.splice($index - 1, 1, target)
+  //         end = false
+  //       }
+  //     }
+  //     $index++
+  //   }
+  //   _index++
+  //   return recursion(callings[_index], origin)
+  // }
+  // const dd = recursion(callings[_index], players)
 }
 
 const players = ["mumu", "soe", "poe", "kai", "mine"]
